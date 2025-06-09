29ed4a3c93c4b238bae593b0700f0f9c56a3095c
Jack Krebsbach <krebsbachjackson@gmail.com>
2025-06-09 14:39:35 -0400

    work on core

---
+++ pkg/core/main.cpp
@@ -0,0 +1,8 @@
+#include "test-function.h"
+#include <iostream>
+
+int main() {
+  double result = phi2(3, 3);
+  std::cout << result;
+  return (0);
+}
---
+++ pkg/core/test-function.h
@@ -0,0 +1,5 @@
+#pragma once
+
+double phi(double t, double a, double eta);
+
+double phi2(double X, double Y);
