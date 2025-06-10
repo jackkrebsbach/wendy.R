98901226df3bd6cce8853e933c9c6ce39d8dea9f
Jack Krebsbach <krebsbachjackson@gmail.com>
2025-06-10 11:33:15 -0400

    switch approach for symbolics

--- exploration.cpp
+++ exploration.cpp
@@ -1,7 +1,10 @@
 #include <Rcpp.h>
 #include <symengine/expression.h>
+#include <symengine/parser.h>
+
 using namespace Rcpp;
 using namespace SymEngine;
+
 using SymEngine::exp;
 using SymEngine::Expression;
 using SymEngine::map_basic_basic;
@@ -25,7 +28,8 @@ void goodwin_2d(Vec &du, const Vec &u, const ParamVec &p, TimeVar &t) {
 }
 
 // [[Rcpp::export]]
-Rcpp::String makeSymbolicSystem(NumericMatrix U, NumericVector p_hat) {
+Rcpp::String makeSymbolicSystem(NumericMatrix U, Rcpp::Function f,
+                                NumericVector p_hat) {
 
   int const J = p_hat.length(); // J Number of parameters
   int const D = U.cols();       // D Dimension of the system
@@ -34,9 +38,24 @@ Rcpp::String makeSymbolicSystem(NumericMatrix U, NumericVector p_hat) {
   auto x = create_symbolic_vars("x", D); // State variables
   auto p = create_symbolic_vars("p", J); // Parameters
   auto t = Expression(symbol("t"));
-  std::vector<Expression> dx(D);
 
-  goodwin_2d(dx, x, p, t);
+  // Convert symbolic variables to character for R
+  CharacterVector x_chr(D), p_chr(J);
+  for (int i = 0; i < D; ++i)
+    x_chr[i] = SymEngine::str(x[i]);
+  for (int i = 0; i < J; ++i)
+    p_chr[i] = SymEngine::str(p[i]);
+
+  std::string t_chr = SymEngine::str(t);
+
+  CharacterVector dx_chr = f(x_chr, p_chr, t_chr);
+  std::vector<std::string> dx_strs = Rcpp::as<std::vector<std::string>>(dx_chr);
+
+  std::vector<Expression> dx(D);
+  for (int i = 0; i < D; ++i) {
+    dx[i] = SymEngine::parse(dx_strs[i]);
+  }
+  // Works with goodwin_2d(dx, x, p, t) need to work with f from Rcpp:Function
 
   map_basic_basic statemap;
   for (int i = 0; i < D; ++i) {
