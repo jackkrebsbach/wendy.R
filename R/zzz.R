e2c0da2f23aefcb9b70f91645d41f80b3529385c
Jack Krebsbach <krebsbachjackson@gmail.com>
2025-06-23 21:01:29 -0600

    required namespace warning

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,6 @@
 .onLoad <- function(libname, pkgname) {
+  if (!requireNamespace("symengine", quietly = TRUE)) {
+    stop("The 'symengine' package is required but not installed. Please install it with install.packages('symengine').")
+  }
   Rcpp::loadModule("WendyR", TRUE)
 }
