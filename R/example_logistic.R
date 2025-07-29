04f772d24a2acb483a61cd714997c8b7bd9d4b8a
Jack Krebsbach <krebsbachjackson@gmail.com>
2025-07-29 12:17:49 -0400

    work on example_logistic

--- R/example_logistic.R
+++ R/example_logistic.R
@@ -1,10 +1,9 @@
 # Setup
 library(symengine)
 library(deSolve)
-
 source('R/WENDy.R')
-
 set.seed(42)
+
 
 # Define RHS
 logistic <- function(u, p, t) {
@@ -14,10 +13,10 @@ logistic <- function(u, p, t) {
 
 # Generate noisy data
 p_star <- c(1, 1)
-u0 <- c(0.01)
+u0 <- c(0.005)
 p0 <- c(0.5, 0.5)
-
 npoints <- 100; t_span <- c(0, 10); t_eval <- seq(t_span[1], t_span[2], length.out = npoints)
+
 
 # Define input for deSolve
 modelODE <- function(tvec, state, parameters) {
@@ -28,15 +27,16 @@ modelODE <- function(tvec, state, parameters) {
 sol <- deSolve::ode( y = u0, times = t_eval, func = modelODE, parms = p_star)
 U <- matrix(c(sol[,2] + 0.05*rnorm(npoints)), ncol = 1)
 tt <- matrix(sol[,1], ncol = 1)
-
-# Plot data
 plot(U, cex = 0.5) 
 
-# Solve for parameters
-data <- WendySolver(logistic, U, p0, tt, compute_svd_ = TRUE)
-p_hat <- data$p_hat
 
-sol_hat <- deSolve::ode( y = u0, times = t_eval, func = modelODE, parms = p_hat)
+# Solve for parameters
+source('R/WENDy.R')
+data <- WendySolver(logistic, U, p0, tt) # WENDy Solve
+
+p_hat <- data$p_hat
+sol_hat <- deSolve::ode(u0, t_eval, modelODE, p_hat)
+
 
 # Compare data and data from estimated parameters
 plot(sol_hat[, 2], cex = 0.5, col = "blue", pch = 16)  
