6a786bab889affd3711b48c5772d74b0906e2d1a
Jack Krebsbach <krebsbachjackson@gmail.com>
2025-08-09 16:06:50 -0400

    dont' vendor symengine

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,22 @@
 .onLoad <- function(libname, pkgname) {
-  if (!requireNamespace("symengine", quietly = TRUE)) {
-    stop(
-      "The 'symengine' package is required but not installed. Please install it with install.packages('symengine')."
-    )
+  # Path to your package's shared object
+  sofile <- system.file("libs", .Platform$r_arch, "wendy.so", package = pkgname)
+  if (sofile == "") {
+    sofile <- system.file("libs", "wendy.so", package = pkgname)
   }
+  # Try to load the shared object
+  tryCatch(
+    dyn.load(sofile),
+    error = function(e) {
+      msg <- paste(
+        "Unable to load the SymEngine C++ library required by the wendy package.\n",
+        "This usually means the SymEngine shared library (libsymengine) could not be found at runtime.\n",
+        "If you installed SymEngine with Conda, make sure your Conda environment is activated before starting R.\n",
+        "If you installed with Homebrew, make sure /usr/local/lib or /opt/homebrew/lib is in your library path.\n",
+        "Original error:\n",
+        conditionMessage(e)
+      )
+      stop(msg, call. = FALSE)
+    }
+  )
 }
